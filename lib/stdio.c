05bdbd4e34483d46d2db10cc250f96db0f633f95
StriveCYX <Scorpio.CYX.4@gmail.com>
2024-12-29 20:01:50 +0800

    Add stdio.h modified stdio.c

--- lib/stdio.c
+++ lib/stdio.c
@@ -1,4 +1,5 @@
 #include "stdint.h"
+#include "stdio.h"
 #define va_start(ap, v) ap = (va_list)&v   // 把ap指向第一个固定参数v 
 #define va_arg(ap, t) *((t*)(ap += 4))   // ap 指向下一个参数并返回其值 
 #define va_end(ap) ap = NULL         // 清除ap  
