17558a31cfdc598ebd29165d2af33dd02e2fe506
StriveCYX <Scorpio.CYX.4@gmail.com>
2025-01-13 11:21:21 +0800

    modified:   lib/user/syscall.c add syscall1

--- lib/user/syscall.c
+++ lib/user/syscall.c
@@ -1,28 +1,40 @@
 #include "syscall.h" 
 /* 无参数的系统调用 */ 
-#define _syscall0(NUMBER) ({             \
-   int retval;                            \
-   asm volatile (                          \
-   "int $0x80"                         \
-   : "=a" (retval)                      \
-   : "a" (NUMBER)                        \
-    : "memory"                            \
-  ) ;
-                                   \
-    retval;                               \
+#define _syscall0(NUMBER) ({                                      \
+   int retval;                                                    \
+   asm volatile (                                                 \
+   "int $0x80"                                                    \
+   : "=a" (retval)                                                \
+   : "a" (NUMBER)                                                 \
+    : "memory"                                                    \
+  ) ;                                                             \
+                                                                  \
+    retval;                                                       \
 }) 
+
+/* 一个参数的系统调用 */
+#define _syscall1(NUMBER, ARG0) ({			                        \
+   int retval;					                                       \
+   asm volatile (					                                    \
+   "pushl %[arg0]; pushl %[number]; int $0x80; addl $8, %%esp"    \
+   : "=a" (retval)					                                 \
+   : [number] "i" (NUMBER), [arg0] "g" (ARG0)		               \
+   : "memory"						                                    \
+   );							                                          \
+   retval;						                                       \
+})
 
  /* 三个参数的系统调用 */ 
  
-#define _syscall3(NUMBER, ARG1, ARG2, ARG3) ({        \
-     int retval;                                              \
-     asm volatile (                                           \
-        "int $0x80"                                           \
-        : "=a" (retval)  \
-        : "a" (NUMBER), "b" (ARG1), "c" (ARG2), "d" (ARG3) \
-        : "memory"          \
-     ) ;                                        \
-     retval;                                          \
+#define _syscall3(NUMBER, ARG1, ARG2, ARG3) ({                    \
+     int retval;                                                  \
+     asm volatile (                                               \
+        "int $0x80"                                               \
+        : "=a" (retval)                                           \
+        : "a" (NUMBER), "b" (ARG1), "c" (ARG2), "d" (ARG3)        \
+        : "memory"                                                \
+     ) ;                                                          \
+     retval;                                                      \
 }) 
 
 /* 打印字符串str */ 
