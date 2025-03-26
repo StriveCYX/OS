37749a983c76e9b53bb660bfc5e9c518923a9155
Strive <Scorpio.CYX.4@gmail.com>
2025-03-26 17:44:34 +0800

    fix some error

--- lib/kernel/stdio-kernel.c
+++ lib/kernel/stdio-kernel.c
@@ -1,4 +1,14 @@
+#include "stdio-kernel.h"
+#include "print.h"
 #include "stdio.h"
+#include "console.h"
+#include "global.h"
+#include "print.h"
+
+#define va_start(ap, v) ap = (va_list) & v // 把ap指向第一个固定参数v
+#define va_arg(ap, t) *((t *)(ap += 4))    // ap指向下一个参数并返回其值
+#define va_end(ap) ap = NULL               // 清除ap
+
 /* 供内核使用的格式化输出函数 */
 void printk(const char* format, ...) {
   va_list args;
--- lib/kernel/stdio-kernel.h
+++ lib/kernel/stdio-kernel.h
@@ -1,2 +1,6 @@
+#ifndef __LIB_KERNEL_STDIOSYS_H
+#define __LIB_KERNEL_STDIOSYS_H
+#include "stdint.h"
 /* 供内核使用的格式化输出函数 */
 void printk(const char* format, ...);
+#endif
